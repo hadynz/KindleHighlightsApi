850321c9901867a15c5b1b02a0d4342f1de9ddea
Hady Osman <hadyos@gmail.com>
2020-04-13 17:02:06 +1200

    Temporary comment out https redirection for local testing

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace KindleHighlightsApi
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
